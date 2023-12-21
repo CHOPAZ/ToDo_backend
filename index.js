9d9e263f2575f33596bd46a8c56ede34f4706940
CHOPAZ <paxa627894@yandex.ru>
2023-12-21 20:30:12 +0500

    create project

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import { getAll, createToDo, updateToDo, removeToDo } from './db.js';
+
+const app = express()
+const port = 3000;
+
+/* прослойка(Middleware) для передачи json в response */
+app.use(express.json())
+
+
+/* Получение всех записей */
+app.get('/todo', async (req, res) => {
+  const items = await getAll()
+  res.send(items.rows);
+})
+
+/* Создать запись */
+app.post('/todo', async (req, res) => {
+  const result = await createToDo(req.body.text, req.body.is_done);
+  res.send(result);
+})
+
+/* Обновление статуса задачи */
+app.put('/todo', async (req, res) => {
+  const result = await updateToDo(req.body.id, req.body.is_done);
+  res.send(result);
+})
+
+/* Удаление задачи */
+app.delete('/todo/:id', async (req, res) => {
+  const result = await removeToDo(req.params.id);
+  res.send(result);
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
