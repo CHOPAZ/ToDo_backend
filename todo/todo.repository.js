4ae650b86ca45df612ca0bc8fa08d4f7035a57ef
CHOPAZ <paxa627894@yandex.ru>
2024-01-16 22:48:16 +0500

    changes todo.repository

--- todo/todo.repository.js
+++ todo/todo.repository.js
@@ -2,22 +2,23 @@ import { client } from "../db.js"
 /* Получение всех записи из таблицы */
 
 class ToDoRepository {
-  async  getAll() {
-    return client.query('SELECT * FROM todo')
+  getAll() {
+    return client.query('SELECT * FROM todo ORDER BY id')
   }
   
   /* Создание задачи */
-  async createToDo(text, is_done) {
+  createToDo(text, is_done) {
     return client.query('INSERT INTO todo (text, is_done) VALUES ($1, $2)', [text, is_done])
   }
   
   /* Обновление статуса задачи */
   async updateToDo(id, is_done) {
-    return client.query('UPDATE todo SET is_done = $1 WHERE id = $2', [is_done, id])
+    const res = await client.query('UPDATE todo SET is_done = $1 WHERE id = $2 RETURNING *', [is_done, id])
+    return res.rows[0]
   }
   
   /* Удаление задачи */
-  async removeToDo(id) {
+  removeToDo(id) {
     return client.query('DELETE FROM todo WHERE id = $1', [id])
   }
 }
